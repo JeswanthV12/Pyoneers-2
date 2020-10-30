1e5b24f90d304cd45cdba0c34c6a0d4d476ecfe6
JeswanthV12 <jveeramusti@gmail.com>
2020-10-30 16:54:22 +0000

    Initial commit

---
+++ Week 1/Homework1.py
@@ -0,0 +1,6 @@
+print("Hi, what is your name?")
+UserName = input()
+print(f"Hi, {UserName}!")
+
+variable = ("Hello, World!")
+print(f"{variable}")
---
+++ Week 1/Project1.py
@@ -0,0 +1,15 @@
+pa = "Pick any"
+noun1sentence = '"WAKE UP SLEEPY HEAD"'
+
+print(f"{pa} past tense verb:")
+verb1 = input()
+print(f"{pa} adjective:")
+adj1 = input()
+print(f"{pa} past tense verb:")
+verb2 = input()
+print(f"{pa} proper noun:")
+noun1 = input()
+print(f"{pa} adjective:")
+adj2 = input()
+
+print(f"When I first got home after school, I {verb1}. It was {adj1}, I {verb2} it. Afterwards, I spent time with {noun1}. He/she is a very {adj2} {noun1}, but I still {verb1} him/her! I took a nap right after, but {noun1} woke me up saying, {noun1sentence}!")
